1dd5b2d18f87c7b15ae259e637f057bf5aa5bbe7
2001-SUHEB <syedsha.suheb2020@vitstudent.ac.in>
2024-04-30 15:47:54 +0530

    Task - 5 completed

---
+++ JSON-iteration.js
@@ -0,0 +1,110 @@
+const ResumeData={
+    "name": "Syed Sha Suheb",
+    "location": "Vellore, Tamilnadu",
+    "phone": "+919080733637",
+    "email": "syedsuheb333@gmail.com",
+    "education": [
+      {
+        "institution": "Vellore Institute of Technology",
+        "degree": "B. Tech",
+        "major": "Computer Science with Specialization in Data Science",
+        "duration": "2020–Present",
+        "cgpa": "7.58",
+        "location": "Vellore, Tamil Nadu"
+      }
+    ],
+    "coursework": {
+      "courses": [
+        "DSA",
+        "DBMS",
+        "Data Visualization",
+        "Programming for Datascience",
+        "Predicive Analytics",
+        "EDA(Exploratory Data Analysis)"
+      ],
+      "languages": ["C", "C++", "JAVASCRIPT", "R", "SQL", "HTML", "CSS"],
+      "technologies": [
+        "Tableau",
+        "VScode",
+        "MongoDB",
+        "Express",
+        "React",
+        "Nodejs",
+        "Bulma",
+        "Bootstrap",
+        "Git",
+        "Github",
+        "Vercel",
+        "Amazon Web Services",
+        "MySQL Wokbench",
+        "Google Sheets"
+      ],
+      "certifications": [
+        "AWS Certified Cloud Practitioner",
+        "Google Data Analytics Professional Certificate"
+      ]
+    },
+    "work_experience": [
+      {
+        "company": "Smartinternz",
+        "position": "Data Analytics Extern",
+        "duration": "May 2023 - July 2023",
+        "location": "Vellore, Tamil Nadu",
+        "responsibilities": [
+          "Collaborated with data analysts to understand the Ford GoBike organization data and the insights required for visualization.",
+          "Assisted in data cleaning, preprocessing, and transformation using MySQL Workbench to ensure accurate and reliable data representation.",
+          "Contributed to the design and implementation of interactive visualizations and dashboards using Tableau Desktop to showcase business analytics trends effectively.",
+          "Integrated the visualizations and dashboards into an interactive website using Bootstrap and VSCode to enhance user engagement.",
+          "Conducted testing and debugging to ensure the website’s functionality and responsiveness across various devices and browsers."
+        ]
+      }
+    ],
+    "projects": [
+      {
+        "title": "Ford-GoBike-Business-Analytics-Trend-Website",
+        "description": "Conducted in-depth data analysis of Ford GoBike organization data using MySQL Workbench to extract valuable insights and trends. Utilized Tableau Desktop to create dynamic and informative visualizations that effectively communicate complex business analytics trends. Developed an interactive and user-friendly website using Bootstrap and VSCode, enhancing the overall user experience and accessibility."
+      },
+      {
+        "title": "Crime-Hotspot-Detection–A-Predictive-Analytics-Project",
+        "description": "Employed advanced machine learning techniques to develop accurate predictive models capable of identifying crime hotspots in the Chicago area. Utilized Python’s data science libraries, such as Pandas, NumPy, and Scikit-learn, to carry out data manipulation and model development. Implemented data visualization techniques to communicate the results effectively and provide actionable insights to stakeholders."
+      },
+      {
+        "title": "Medical-Insurance-Fraud-Detection",
+        "description": "Processed and prepared the medical insurance dataset, ensuring data integrity and consistency for training the fraud detection model. Developed and fine-tuned a machine learning model capable of identifying fraudulent insurance claims with high accuracy and precision. Utilized Python’s data science libraries, including Pandas, Scikit-learn, and TensorFlow, to implement the fraud detection model."
+      }
+    ],
+    "achievements": [
+      {
+        "title": "Publication (Conference)",
+        "conference": "2023 3rd International Conference on Innovative Mechanisms for Industry Applications (ICIMIA)",
+        "description": "Developed an IoT-based Smart Drain Monitoring System presented at the 2023 ICIMIA. Titled ”IoT-based Smart Drain Monitoring System with Real-Time Alert Messages and Data Analysis.” Acknowledged for significant contributions to innovative mechanisms in the realm of industry applications."
+      }
+    ]
+  }
+  
+  /////////////////////////////JSON ITERATION USING FOR IN LOOP/////////////////////////////////
+
+//   for(var key in ResumeData){
+//     console.log(key+":"+ResumeData[key]);
+//   }
+
+/////////////////////////////JSON ITERATION USING FOREACH LOOP(WORKS ONLY ON ARRAYAS)/////////////////////////////////
+
+// ResumeData.education.forEach(item => {
+//     console.log('Education:', item);
+//   });
+
+////////////////////////////JSON ITERATION USING FOR of LOOP(WORKS ONLY ON ARRAYAS)/////////////////////////////////
+
+// for (let experience of ResumeData.work_experience) {
+//     console.log('Work Experience:', experience);
+//   }
+
+
+////////////////////////////JSON ITERATION USING NORMAL FOR LOOP//////////////////////////////////////////////////
+
+// const resumeKeys = Object.keys(ResumeData);
+// for (let i = 0; i < resumeKeys.length; i++) {
+//   const key = resumeKeys[i];
+//   console.log(key + ':', ResumeData[key]);
+// }
